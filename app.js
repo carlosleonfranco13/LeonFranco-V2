ee4665083826aa2d3c9519484474c7a60505eade
carlos <carlosleonfranco13@gmail.com>
2023-03-29 22:45:05 -0500

    Cambio de color mediante toggle a body y .overlay

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-const toggleIcon = document.querySelector('.toggle-icon');
+const overlay = document.querySelector(".overlay");
+const body = document.querySelector("body");
+const toggleIcon = document.querySelector(".toggle-icon");
+let overlayColorOrig = 'var(--color-1)';
+let bodyColorOrig = '#fff'
 
-toggleIcon.addEventListener('click', () => {
-    toggleIcon.classList.toggle('bx-sun');
-})
+toggleIcon.addEventListener("click", () => {
+  toggleIcon.classList.toggle("bx-sun");
+  if (toggleIcon.classList.contains("bx-sun")) {
+    body.style.background = "#030A15";
+    overlay.style.borderTopColor = "white";
+    overlay.style.borderLeftColor = "white";
+    overlay.style.borderBottomColor = "white";
+  } else {
+    overlay.style.borderTopColor = overlayColorOrig;
+    overlay.style.borderLeftColor = overlayColorOrig;
+    overlay.style.borderBottomColor = overlayColorOrig;
+    body.style.background = bodyColorOrig;
+  }
+});
