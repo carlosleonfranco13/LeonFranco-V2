55952c24e3c4620801ca5981e35c45118a9e0693
carlos <carlosleonfranco13@gmail.com>
2023-03-30 21:00:07 -0500

    Linea de tiempo work

---
+++ app.js
@@ -0,0 +1,15 @@
+const bgHeader = document.querySelector('.header');
+
+const headerHeight = bgHeader.offsetHeight;
+
+window.addEventListener('scroll', () => {
+    const scrollPos = window.scrollY;
+
+    if(scrollPos > headerHeight) {
+        bgHeader.classList.add('active');
+        bgHeader.classList.remove('transparent');
+    } else {
+        bgHeader.classList.remove('active');
+        bgHeader.classList.add('transparent');
+    }
+});
